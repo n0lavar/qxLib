412ae4fa15d12dbfec959360866402087818eca3
Nick Khrapov <nick.khrapov@gmail.com>
2020-09-12 01:24:15 +0300

    constexpr_counter: add value(), comments, tests

--- include/qx/other/constexpr_conter.h
+++ include/qx/other/constexpr_conter.h
@@ -36,7 +36,7 @@ template <typename T> class Flag
         }
     };
 
-    template <class Dummy, int = (adl_flag(Dummy{}), 0) >
+    template <class Dummy, int = (adl_flag(Dummy{}), 0)>
     static constexpr bool Check(int)
     {
         return true;
@@ -86,7 +86,7 @@ struct Checker
 template<typename T, int N, int Step>
 struct CheckerWrapper
 {
-    template<bool B = Flag<Tag<T, N>>{}.Read(), int M = Checker<T, N, Step, B>{}.currentval() >
+    template<bool B = Flag<Tag<T, N>>{}.Read(), int M = Checker<T, N, Step, B>{}.currentval()>
     static constexpr int currentval()
     {
         return M;
@@ -96,19 +96,10 @@ struct CheckerWrapper
 template<typename T, int N, int Step>
 struct Checker<T, N, Step, true>
 {
-    template<int M = CheckerWrapper<T, N + Step, Step>{}.currentval() >
+    template<int M = CheckerWrapper<T, N + Step, Step>{}.currentval()>
     static constexpr int currentval() noexcept
     {
         return M;
-    }
-};
-
-template<typename T, int N, bool B = Flag<Tag<T, N>>{}.ReadSet() >
-struct Next
-{
-    static constexpr int value() noexcept
-    {
-        return N;
     }
 };
 
@@ -116,26 +107,53 @@ struct Next
 
 //==============================================================================
 //
-//!\class           qx::constexpr_counter<Tag, Start, Step>
+//!\class        qx::constexpr_counter<CounterTag, Start, Step>
 //
 //!\brief   constexpr counter. use unique tag for different instances
 //!\details modyfied version of Anthony Williams constexpr counter
 //          https://stackoverflow.com/a/58200261/8021662
 //          \see exapmles in tests\test_constexpr_counter.cpp
+//          \note that you can't vary Start and Step values with same Tag
+//          prefer using "using my_counter = qx::constexpr_counter<struct MyTag, Start, Step>
 //
 //!\author  Khrapov
 //!\date    25.08.2020
 //
 //==============================================================================
-template <class Tag = void, int Start = 0, int Step = 1>
+template <class CounterTag = void, int Start = 0, int Step = 1>
 class constexpr_counter
 {
 public:
-    template <int N = detail::CheckerWrapper<Tag, Start, Step>{}.currentval()>
-    static constexpr int next()
-    {
-        return detail::Next<Tag, N>{}.value();
-    }
+
+//==============================================================================
+//!\fn      qx::constexpr_counter<CounterTag, Start, Step>::value<N>
+//
+//!\brief  Get current counter value
+//!\retval  - current counter value
+//!\author Khrapov
+//!\date   12.09.2020
+//==============================================================================
+template <int  N = detail::CheckerWrapper<CounterTag, Start, Step>{}.currentval()>
+static constexpr int value(void) noexcept
+{
+    return N;
+}
+
+//==============================================================================
+//!\fn     qx::constexpr_counter<CounterTag, Start, Step>::next<N, B>
+//
+//!\brief  Increase value by Step and return new value
+//!\retval  - increased value
+//!\author Khrapov
+//!\date   12.09.2020
+//==============================================================================
+template <int  N = detail::CheckerWrapper<CounterTag, Start, Step>{}.currentval(),
+          bool B = detail::Flag<detail::Tag<CounterTag, N>>{}.ReadSet()>
+static constexpr int next(void) noexcept
+{
+    return N + Step;
+}
+
 };
 
 }
