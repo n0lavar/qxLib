cbb38a464abcaef0c3513bb5652b2d6c144d1d1c
n0lavar <nick.khrapov@gmail.com>
2021-08-18 15:21:36 +0300

    format string_utils

--- include/qx/containers/string_utils.h
+++ include/qx/containers/string_utils.h
@@ -1,15 +1,12 @@
-//==============================================================================
-//
-//!\file                         string_utils.h
-//
-//!\brief       String functions
-//!\details     ~
-//
-//!\author      Khrapov
-//!\date        17.10.2020
-//!\copyright   (c) Nick Khrapov, 2020. All right reserved.
-//
-//==============================================================================
+/**
+
+    @file      string_utils.h
+    @brief     String functions
+    @author    Khrapov
+    @date      17.10.2020
+    @copyright © Nick Khrapov, 2021. All right reserved.
+
+**/
 #pragma once
 
 #include <qx/typedefs.h>
@@ -17,43 +14,37 @@
 namespace qx
 {
 
-//==============================================================================
-//!\fn                        qx::djb2a_hash<Char>
-//
-//!\brief djb2a hash
-//!\param  pszStr - string for hashing
-//!\param  nSeed  - seed for hashing
-//!\param  nLen   - string length
-//!\retval        - 32bit unsigned value
-//!\author Khrapov
-//!\date   25.01.2021
-//==============================================================================
-template<typename Char>
-constexpr u32 djb2a_hash(const Char* pszStr, u32 nSeed, size_t nLen)
+/**
+    @brief  djb2a hash
+    @tparam value_type - char type
+    @param  pStr       - string for hashing
+    @param  nSeed      - seed for hashing
+    @param  nLen       - string length
+    @retval            - 32bit unsigned value
+**/
+template<typename value_type>
+constexpr u32 djb2a_hash(const value_type* pStr, u32 nSeed, size_t nLen)
 {
     u32 nHash = nSeed;
 
     for (size_t i = 0; i < nLen; i++)
-        nHash = nHash * 33 ^ pszStr[i];
+        nHash = nHash * 33 ^ pStr[i];
 
     return nHash;
 }
 
-//==============================================================================
-//!\fn                        qx::djb2a_hash<Char>
-//
-//!\brief djb2a hash
-//!\param  pszStr - string for hashing
-//!\param  nSeed  - seed for hashing
-//!\retval        - 32bit unsigned value
-//!\author Khrapov
-//!\date   25.01.2021
-//==============================================================================
-template<typename Char>
-constexpr u32 djb2a_hash(const Char* pszStr, u32 nSeed)
+/**
+    @brief  djb2a hash
+    @tparam value_type - char type
+    @param  pszStr     - string for hashing (zero terminated)
+    @param  nSeed      - seed for hashing
+    @retval            - 32bit unsigned value
+**/
+template<typename value_type>
+constexpr u32 djb2a_hash(const value_type* pszStr, u32 nSeed)
 {
-    u32 nHash = nSeed;
-    Char ch;
+    u32        nHash = nSeed;
+    value_type ch;
 
     while ((ch = *pszStr++) != 0)
         nHash = ((nHash << 5) + nHash) + ch;
@@ -61,22 +52,21 @@ constexpr u32 djb2a_hash(const Char* pszStr, u32 nSeed)
     return nHash;
 }
 
-//==============================================================================
-//!\fn                      qx::murmur_32_hash<Char>
-//
-//!\brief   Murmur nHash
-//!\details https://en.wikipedia.org/wiki/MurmurHash
-//          https://softwareengineering.stackexchange.com/questions/49550/which-hashing-algorithm-is-best-for-uniqueness-and-speed
-//
-//!\param   pszStr - string for hashing (char must be 1, 2 or 4 bytes long)
-//!\param   nSeed  - seed for hashing
-//!\param   nLen   - string length
-//!\retval         - 32bit unsigned value
-//!\author  Khrapov
-//!\date    10.09.2020
-//==============================================================================
-template<typename Char>
-constexpr u32 murmur_32_hash(const Char* pszStr, u32 nSeed, size_t nLen) noexcept
+/**
+    @brief   Murmur nHash
+    @details https://en.wikipedia.org/wiki/MurmurHash
+             https://softwareengineering.stackexchange.com/questions/49550/which-hashing-algorithm-is-best-for-uniqueness-and-speed
+    @tparam  value_type - char type
+    @param   pStr       - string for hashing
+    @param   nSeed      - seed for hashing
+    @param   nLen       - string length
+    @retval             - 32bit unsigned value
+**/
+template<typename value_type>
+constexpr u32 murmur_32_hash(
+    const value_type* pStr,
+    u32               nSeed,
+    size_t            nLen) noexcept
 {
     u32 nHash = nSeed;
 
@@ -88,28 +78,32 @@ constexpr u32 murmur_32_hash(const Char* pszStr, u32 nSeed, size_t nLen) noexcep
             u32 k = 0;
 
             // constexpr version of std::memcpy(&k, pszStr, sizeof(u32));
-            if constexpr (sizeof(Char) == sizeof(u32))
+            static_assert(
+                sizeof(value_type) == 1 || sizeof(value_type) == 2
+                || sizeof(value_type) == 4);
+
+            if constexpr (sizeof(value_type) == sizeof(u32))
             {
-                k += *(pszStr + 0);
+                k += *(pStr + 0);
             }
-            else if constexpr (sizeof(Char) == sizeof(u16))
+            else if constexpr (sizeof(value_type) == sizeof(u16))
             {
-                k += *(pszStr + 0);
+                k += *(pStr + 0);
                 k <<= sizeof(u16);
-                k += *(pszStr + 1);
+                k += *(pStr + 1);
             }
             else
             {
-                k += *(pszStr + 0);
+                k += *(pStr + 0);
                 k <<= sizeof(u8);
-                k += *(pszStr + 1);
+                k += *(pStr + 1);
                 k <<= sizeof(u8);
-                k += *(pszStr + 2);
+                k += *(pStr + 2);
                 k <<= sizeof(u8);
-                k += *(pszStr + 3);
+                k += *(pStr + 3);
             }
 
-            pszStr += sizeof(u32);
+            pStr += sizeof(u32);
 
             k *= 0xcc9e2d51;
             k = (k << 15) | (k >> 17);
@@ -124,11 +118,11 @@ constexpr u32 murmur_32_hash(const Char* pszStr, u32 nSeed, size_t nLen) noexcep
     if (nLen & 3)
     {
         size_t i = nLen & 3;
-        u32 k = 0;
+        u32    k = 0;
         do
         {
             k <<= 8;
-            k |= pszStr[i - 1];
+            k |= pStr[i - 1];
         } while (--i);
         k *= 0xcc9e2d51;
         k = (k << 15) | (k >> 17);
@@ -146,19 +140,18 @@ constexpr u32 murmur_32_hash(const Char* pszStr, u32 nSeed, size_t nLen) noexcep
     return nHash;
 }
 
-//==============================================================================
-//!\fn                  qx::iter_strcmp<FwdIt1, FwdIt2>
-//
-//!\brief  Compares string 1 with string 2
-//!\param  itBegin1 - string 1 begin iterator
-//!\param  itEnd1   - string 1 end iterator
-//!\param  itBegin2 - string 2 begin iterator
-//!\param  itEnd2   - string 2 end iterator
-//!\retval          - < 0 the first character that does not match has a lower value in str 1 than in str 2
-//                      0 the contents of both strings are equal
-//                    > 0 the first character that does not match has a greater value in str 1 than in str 2
-//!\date   23.10.2020
-//==============================================================================
+/**
+    @brief  Compares string 1 with string 2
+    @tparam FwdIt1   - string 1 iterator type
+    @tparam FwdIt2   - string 2 iterator type
+    @param  itBegin1 - string 1 begin iterator 
+    @param  itEnd1   - string 1 end iterator 
+    @param  itBegin2 - string 2 begin iterator 
+    @param  itEnd2   - string 2 end iterator 
+    @retval          - < 0 the first character that does not match has a lower value in str 1 than in str 2
+                         0 the contents of both strings are equal
+                       > 0 the first character that does not match has a greater value in str 1 than in str 2
+**/
 template<class FwdIt1, class FwdIt2>
 constexpr int iter_strcmp(
     FwdIt1 itBegin1,
@@ -183,43 +176,17 @@ constexpr int iter_strcmp(
     return nRet;
 }
 
-//==============================================================================
-//!\fn                            qx::strlen
-//
-//!\brief  Constexpr string length
-//!\param  psz - poiter to string zero terminated
-//!\retval     - string length
-//!\author Khrapov
-//!\date   24.09.2020
-//==============================================================================
-template<typename TChar>
-constexpr std::size_t strlen(const TChar* psz)
-{
-    std::size_t nLen = 0;
-    while (psz && *psz != 0)
-    {
-        ++psz;
-        nLen++;
-    }
-
-    return nLen;
-}
-
-//==============================================================================
-//!\fn                         qx::strcmp<TChar>
-//
-//!\brief  Compare two strings
-//!\param  pszLeft  - first string
-//!\param  pszRight - second string
-//!\retval          -
-//  < 0 the first character that does not match has a lower value in ptr1 than in ptr2
-//    0 the contents of both strings are equal
-//  > 0 the first character that does not match has a greater value in ptr1 than in ptr2
-//!\author Khrapov
-//!\date   25.09.2020
-//==============================================================================
-template<typename TChar>
-constexpr int strcmp(const TChar* pszLeft, const TChar* pszRight)
+/**
+    @brief  Constexpr compare two strings
+    @tparam value_type - char type
+    @param  pszLeft    - first string (zero terminated)
+    @param  pszRight   - second string (zero terminated)
+    @retval            -  < 0 the first character that does not match has a lower value in ptr1 than in ptr2
+                            0 the contents of both strings are equal
+                          > 0 the first character that does not match has a greater value in ptr1 than in ptr2
+**/
+template<typename value_type>
+constexpr int strcmp(const value_type* pszLeft, const value_type* pszRight)
 {
     while (*pszLeft && (*pszLeft == *pszRight))
     {
@@ -231,53 +198,75 @@ constexpr int strcmp(const TChar* pszLeft, const TChar* pszRight)
 
 namespace detail
 {
-    template<typename value_type>
-    constexpr const value_type* const choose_str_prefix(const char* const, const wchar_t* const) noexcept;
 
-    template<>
-    constexpr const char* const choose_str_prefix<char>(const char* const c, const wchar_t* const) noexcept
-    {
-        return c;
-    }
+template<typename value_type>
+constexpr const value_type* const choose_str_prefix(
+    const char* const,
+    const wchar_t* const) noexcept;
 
-    template<>
-    constexpr const wchar_t* const choose_str_prefix<wchar_t>(const char* const, const wchar_t* const w) noexcept
-    {
-        return w;
-    }
-
-    template<typename value_type>
-    constexpr value_type choose_char_prefix(char, wchar_t) noexcept;
-
-    template<>
-    constexpr char choose_char_prefix<char>(char c, wchar_t) noexcept
-    {
-        return c;
-    }
-
-    template<>
-    constexpr wchar_t choose_char_prefix<wchar_t>(char, wchar_t w) noexcept
-    {
-        return w;
-    }
+template<>
+constexpr const char* const choose_str_prefix<char>(
+    const char* const c,
+    const wchar_t* const) noexcept
+{
+    return c;
 }
 
+template<>
+constexpr const wchar_t* const choose_str_prefix<wchar_t>(
+    const char* const,
+    const wchar_t* const w) noexcept
+{
+    return w;
 }
+
+template<typename value_type>
+constexpr value_type choose_char_prefix(char, wchar_t) noexcept;
+
+template<>
+constexpr char choose_char_prefix<char>(char c, wchar_t) noexcept
+{
+    return c;
+}
+
+template<>
+constexpr wchar_t choose_char_prefix<wchar_t>(char, wchar_t w) noexcept
+{
+    return w;
+}
+
+} // namespace detail
+
+} // namespace qx
 
 //==============================================================================
 
 #define _QX_TO_WSTRING(x) L##x
-#define QX_TO_WSTRING(x) _QX_TO_WSTRING(x)
 
-/*
-    chose witch of prefixes add to string : L or none
-*/
-#define QX_STR_PREFIX(value_type, str) qx::detail::choose_str_prefix<value_type>(str, QX_TO_WSTRING(str))
+/**
+    @macro QX_TO_WSTRING
+    @brief Convert char* to wchar_t*
+    @param str - string to apply
+**/
+#define QX_TO_WSTRING(str) _QX_TO_WSTRING(str)
 
-/*
-    chose witch of prefixes add to char : L or none
-*/
-#define QX_CHAR_PREFIX(value_type, ch) qx::detail::choose_char_prefix<value_type>(ch, QX_TO_WSTRING(ch))
+/**
+    @macro QX_STR_PREFIX
+    @brief Chose witch of prefixes add to string : L or none
+    @param value_type - char type
+    @param str        - string to apply
+**/
+#define QX_STR_PREFIX(value_type, str) \
+    qx::detail::choose_str_prefix<value_type>(str, QX_TO_WSTRING(str))
+
+/**
+    @macro QX_CHAR_PREFIX
+    @brief Chose witch of prefixes add to char : L or none
+    @param value_type - char type
+    @param ch         - string to apply
+**/
+#define QX_CHAR_PREFIX(value_type, ch) \
+    qx::detail::choose_char_prefix<value_type>(ch, QX_TO_WSTRING(ch))
 
 //==============================================================================
 
@@ -286,9 +275,9 @@ namespace detail
 */
 #define QX_STATIC_ASSERT_STR_EQ(a, b) static_assert(qx::strcmp((a), (b)) == 0)
 #define QX_STATIC_ASSERT_STR_NE(a, b) static_assert(qx::strcmp((a), (b)) != 0)
-#define QX_STATIC_ASSERT_STR_LT(a, b) static_assert(qx::strcmp((a), (b)) <  0)
+#define QX_STATIC_ASSERT_STR_LT(a, b) static_assert(qx::strcmp((a), (b)) < 0)
 #define QX_STATIC_ASSERT_STR_LE(a, b) static_assert(qx::strcmp((a), (b)) <= 0)
-#define QX_STATIC_ASSERT_STR_GT(a, b) static_assert(qx::strcmp((a), (b)) >  0)
+#define QX_STATIC_ASSERT_STR_GT(a, b) static_assert(qx::strcmp((a), (b)) > 0)
 #define QX_STATIC_ASSERT_STR_GE(a, b) static_assert(qx::strcmp((a), (b)) >= 0)
 
 //==============================================================================
@@ -296,14 +285,12 @@ namespace detail
 namespace qx
 {
 
-//==============================================================================
-//!\fn                qx::get_format_specifier<value_type, T>
-//
-//!\brief  Get format specifier for type
-//!\retval  - format specifier or nullptr if type is not supported
-//!\author Khrapov
-//!\date   11.09.2020
-//==============================================================================
+/**
+    @brief  Get format specifier for type
+    @tparam value_type - char type
+    @tparam T          - target type
+    @retval            - format specifier or nullptr
+**/
 template<typename value_type, typename T>
 constexpr auto get_format_specifier(void) noexcept
 {
@@ -373,4 +360,23 @@ constexpr auto get_format_specifier(void) noexcept
     return pszFormat;
 }
 
+/**
+    @brief  Constexpr string length
+    @tparam value_type - char type
+    @param  psz        - pointer to string zero terminated
+    @retval            - string length
+**/
+template<typename value_type>
+constexpr std::size_t strlen(const value_type* psz)
+{
+    std::size_t nLen = 0;
+    while (psz && *psz != QX_CHAR_PREFIX(value_type, '\0'))
+    {
+        ++psz;
+        nLen++;
+    }
+
+    return nLen;
 }
+
+} // namespace qx
