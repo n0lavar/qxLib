e90febc0091980aa2133c850051e40519e5007a7
n0lavar <nick.khrapov@gmail.com>
2021-08-18 18:09:05 +0300

    format easing_functions

--- include/qx/easing/easing_functions.h
+++ include/qx/easing/easing_functions.h
@@ -1,20 +1,18 @@
-//==============================================================================
-//
-//!\file                       easing_functions.h
-//
-//!\brief       File contains easing functions
-//!\details     See https://doc.magnum.graphics/magnum/namespaceMagnum_1_1Animation_1_1Easing.html
-//              for details
-//              For all y = f(x):
-//              0.0 <= x <= 1.0
-//              f(0.0) = 0.0 (with epsilon = qx::easing::eps<T>())
-//              f(1.0) = 1.0 (with epsilon = qx::easing::eps<T>())
-//
-//!\author      Khrapov
-//!\date        4.05.2021
-//!\copyright   (c) Nick Khrapov, 2021. All right reserved.
-//
-//==============================================================================
+/**
+
+    @file      easing_functions.h
+    @brief     File contains easing functions
+    #details   See https://doc.magnum.graphics/magnum/namespaceMagnum_1_1Animation_1_1Easing.html
+               for details
+               For all y = f(x):
+               0.0 <= x <= 1.0
+               f(0.0) = 0.0 (with epsilon = qx::easing::eps<T>())
+               f(1.0) = 1.0 (with epsilon = qx::easing::eps<T>())
+    @author    Khrapov
+    @date      4.05.2021
+    @copyright © Nick Khrapov, 2021. All right reserved.
+
+**/
 #pragma once
 
 #include <qx/math.h>
@@ -26,12 +24,12 @@
 namespace qx::easing
 {
 
-//-------------------------------- function type -------------------------------
+// ------------------------------- function type -------------------------------
 
 template<typename T>
 using func = std::function<T(T)>;
 
-//----------------------- epsilon for borders guarantees -----------------------
+// ---------------------- epsilon for borders guarantees -----------------------
 
 template<typename T>
 constexpr T eps(void)
@@ -45,7 +43,7 @@ constexpr float eps(void)
     return 1e-6f;
 }
 
-//------------------------------ easing functions ------------------------------
+// ----------------------------- easing functions ------------------------------
 
 template<typename T>
 inline T linear_func(T x) noexcept
@@ -59,9 +57,7 @@ constexpr auto linear = linear_func<float>;
 template<typename T>
 inline T step_func(T x) noexcept
 {
-    return x < T(0.5f)
-        ? T(0.0f)
-        : T(1.0f);
+    return x < T(0.5f) ? T(0.0f) : T(1.0f);
 }
 constexpr auto step = step_func<float>;
 
@@ -218,7 +214,9 @@ constexpr auto quintic_in_out = quintic_in_out_func<float>;
 template<typename T>
 inline T sine_in_func(T x) noexcept
 {
-    return T(1.0f) + static_cast<T>(std::sin(std::numbers::pi_v<T> / T(2.0f) * (x - T(1.0f))));
+    return T(1.0f)
+           + static_cast<T>(
+               std::sin(std::numbers::pi_v<T> / T(2.0f) * (x - T(1.0f))));
 }
 constexpr auto sine_in = sine_in_func<float>;
 
@@ -236,7 +234,8 @@ constexpr auto sine_out = sine_out_func<float>;
 template<typename T>
 inline T sine_in_out_func(T x) noexcept
 {
-    return T(0.5f) * (T(1.0f) - static_cast<T>(std::cos(x * std::numbers::pi_v<T>)));
+    return T(0.5f)
+           * (T(1.0f) - static_cast<T>(std::cos(x * std::numbers::pi_v<T>)));
 }
 constexpr auto sine_in_out = sine_in_out_func<float>;
 
@@ -264,9 +263,18 @@ template<typename T>
 inline T circular_in_out_func(T x) noexcept
 {
     if (x < T(0.5f))
-        return T(0.5f) * (T(1.0f) - std::sqrt(T(1.0f) - T(4.0f) * std::pow(x, T(2.0f))));
+    {
+        return T(0.5f)
+               * (T(1.0f)
+                  - std::sqrt(T(1.0f) - T(4.0f) * std::pow(x, T(2.0f))));
+    }
     else
-        return T(0.5f) * (T(1.0f) + std::sqrt(-T(4.0f) * std::pow(x, T(2.0f)) + T(8.0f) * x - T(3.0f)));
+    {
+        return T(0.5f)
+               * (T(1.0f)
+                  + std::sqrt(
+                      -T(4.0f) * std::pow(x, T(2.0f)) + T(8.0f) * x - T(3.0f)));
+    }
 }
 constexpr auto circular_in_out = circular_in_out_func<float>;
 
@@ -275,9 +283,7 @@ constexpr auto circular_in_out = circular_in_out_func<float>;
 template<typename T>
 inline T exponential_in_func(T x) noexcept
 {
-    return x <= T(0.0f)
-        ? T(0.0f)
-        : std::pow(T(2.0f), T(10.0f) * (x - T(1.0f)));
+    return x <= T(0.0f) ? T(0.0f) : std::pow(T(2.0f), T(10.0f) * (x - T(1.0f)));
 }
 constexpr auto exponential_in = exponential_in_func<float>;
 
@@ -286,9 +292,7 @@ constexpr auto exponential_in = exponential_in_func<float>;
 template<typename T>
 inline T exponential_out_func(T x) noexcept
 {
-    return x >= T(1.0f)
-        ? T(1.0f)
-        : T(1.0f) - std::pow(T(2.0f), -T(10.0f) * x);
+    return x >= T(1.0f) ? T(1.0f) : T(1.0f) - std::pow(T(2.0f), -T(10.0f) * x);
 }
 constexpr auto exponential_out = exponential_out_func<float>;
 
@@ -314,7 +318,8 @@ template<typename T>
 inline T elastic_in_func(T x) noexcept
 {
     return std::pow(T(2.0f), T(10.0f) * (x - T(1.0f)))
-        * static_cast<T>(std::sin(T(13.0f) * std::numbers::pi_v<T> / T(2.0f) * x));
+           * static_cast<T>(
+               std::sin(T(13.0f) * std::numbers::pi_v<T> / T(2.0f) * x));
 }
 constexpr auto elastic_in = elastic_in_func<float>;
 
@@ -323,8 +328,11 @@ constexpr auto elastic_in = elastic_in_func<float>;
 template<typename T>
 inline T elastic_out_func(T x) noexcept
 {
-    return T(1.0f) - std::pow(T(2.0f), -T(10.0f) * x)
-        * static_cast<T>(std::sin(T(13.0f) * std::numbers::pi_v<T> / T(2.0f) * (x + T(1.0f))));
+    return T(1.0f)
+           - std::pow(T(2.0f), -T(10.0f) * x)
+                 * static_cast<T>(std::sin(
+                     T(13.0f) * std::numbers::pi_v<T> / T(2.0f)
+                     * (x + T(1.0f))));
 }
 constexpr auto elastic_out = elastic_out_func<float>;
 
@@ -336,12 +344,14 @@ inline T elastic_in_out_func(T x) noexcept
     if (x < T(0.5f))
     {
         return T(0.5f) * std::pow(T(2.0f), T(10.0f) * (T(2.0f) * x - T(1.0f)))
-            * static_cast<T>(std::sin(T(13.0f) * std::numbers::pi_v<T> * x));
+               * static_cast<T>(std::sin(T(13.0f) * std::numbers::pi_v<T> * x));
     }
     else
     {
-        return T(1.0f) - T(0.5f) * std::pow(T(2.0f), T(10.0f) * (T(1.0f) - T(2.0f) * x))
-            * static_cast<T>(std::sin(T(13.0f) * std::numbers::pi_v<T> * x));
+        return T(1.0f)
+               - T(0.5f) * std::pow(T(2.0f), T(10.0f) * (T(1.0f) - T(2.0f) * x))
+                     * static_cast<T>(
+                         std::sin(T(13.0f) * std::numbers::pi_v<T> * x));
     }
 }
 constexpr auto elastic_in_out = elastic_in_out_func<float>;
@@ -351,7 +361,9 @@ constexpr auto elastic_in_out = elastic_in_out_func<float>;
 template<typename T>
 inline T back_in_func(T x) noexcept
 {
-    return x * (std::pow(x, T(2.0f)) - static_cast<T>(std::sin(std::numbers::pi_v<T> * x)));
+    return x
+           * (std::pow(x, T(2.0f))
+              - static_cast<T>(std::sin(std::numbers::pi_v<T> * x)));
 }
 constexpr auto back_in = back_in_func<float>;
 
@@ -361,7 +373,10 @@ template<typename T>
 inline T back_out_func(T x) noexcept
 {
     const T fInv = T(1.0f) - x;
-    return T(1.0f) - fInv * (fInv * fInv - static_cast<T>(std::sin(std::numbers::pi_v<T> * fInv)));
+    return T(1.0f)
+           - fInv
+                 * (fInv * fInv
+                    - static_cast<T>(std::sin(std::numbers::pi_v<T> * fInv)));
 }
 constexpr auto back_out = back_out_func<float>;
 
@@ -374,13 +389,17 @@ inline T back_in_out_func(T x) noexcept
     {
         const T f2t = T(2.0f) * x;
         return T(0.5f) * f2t
-            * (f2t * f2t - static_cast<T>(std::sin(std::numbers::pi_v<T> * f2t)));
+               * (f2t * f2t
+                  - static_cast<T>(std::sin(std::numbers::pi_v<T> * f2t)));
     }
     else
     {
         const T fInv = T(2.0f) - T(2.0f) * x;
-        return T(1.0f) - T(0.5f) * fInv
-            * (fInv * fInv - static_cast<T>(std::sin(std::numbers::pi_v<T> * fInv)));
+        return T(1.0f)
+               - T(0.5f) * fInv
+                     * (fInv * fInv
+                        - static_cast<T>(
+                            std::sin(std::numbers::pi_v<T> * fInv)));
     }
 }
 constexpr auto back_in_out = back_in_out_func<float>;
@@ -391,13 +410,24 @@ template<typename T>
 inline T bounce_out_func(T x) noexcept
 {
     if (x < T(4.0f) / T(11.0f))
+    {
         return (T(121.0f) * std::pow(x, T(2.0f))) / T(16.0f);
+    }
     else if (x < T(8.0f) / T(11.0f))
-        return T(363.0f) / T(40.0f) * std::pow(x, T(2.0f)) - T(99.0f) / T(10.0f) * x + T(17.0f) / T(5.0f);
+    {
+        return T(363.0f) / T(40.0f) * std::pow(x, T(2.0f))
+               - T(99.0f) / T(10.0f) * x + T(17.0f) / T(5.0f);
+    }
     else if (x < T(9.0f) / T(10.0f))
-        return T(4356.0f) / T(361.0f) * std::pow(x, T(2.0f)) - T(35442.0f) / T(1805.0f) * x + T(16061.0f) / T(1805.0f);
+    {
+        return T(4356.0f) / T(361.0f) * std::pow(x, T(2.0f))
+               - T(35442.0f) / T(1805.0f) * x + T(16061.0f) / T(1805.0f);
+    }
     else
-        return T(54.0f) / T(5.0f) * std::pow(x, T(2.0f)) - T(513.0f) / T(25.0f) * x + T(268.0f) / T(25.0f);
+    {
+        return T(54.0f) / T(5.0f) * std::pow(x, T(2.0f))
+               - T(513.0f) / T(25.0f) * x + T(268.0f) / T(25.0f);
+    }
 }
 constexpr auto bounce_out = bounce_out_func<float>;
 
@@ -422,4 +452,4 @@ inline T bounce_in_out_func(T x) noexcept
 }
 constexpr auto bounce_in_out = bounce_in_out_func<float>;
 
-}
+} // namespace qx::easing
