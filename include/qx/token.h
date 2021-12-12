04316fd063dc90c8fc6124990d1611470450e651
n0lavar <nick.khrapov@gmail.com>
2021-12-12 19:01:32 +0300

    add qx::token class

---
+++ include/qx/token.h
@@ -0,0 +1,56 @@
+/**
+
+    @file      token.h
+    @brief     Contains qx::token class
+    @author    Khrapov
+    @date      12.12.2021
+    @copyright © Nick Khrapov, 2021. All right reserved.
+
+**/
+#pragma once
+
+#include <qx/useful_macros.h>
+
+namespace qx
+{
+
+/**
+
+    @class   token
+    @brief   Token class for RAII
+    @details Functor passed in constructor will be called in destructor
+    @author  Khrapov
+    @date    12.12.2021
+
+**/
+class token
+{
+public:
+    using destroyer = std::function<void(void)>;
+
+public:
+    QX_NONCOPYABLE(token);
+    QX_MOVABLE(token);
+
+    /**
+        @brief token object constructor
+        @param _destroyer - functor that will be called when object will be destroyed
+    **/
+    token(destroyer _destroyer) : m_Destroyer(std::move(_destroyer))
+    {
+    }
+
+    /**
+        @brief token object destructor
+    **/
+    ~token(void)
+    {
+        if (m_Destroyer)
+            m_Destroyer();
+    }
+
+private:
+    destroyer m_Destroyer;
+};
+
+} // namespace qx
