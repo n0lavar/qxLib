3ced2da393377c0cca83068677da40da0de4c83b
n0lavar <nick.khrapov@gmail.com>
2022-08-23 02:08:00 +0300

    add iterate function

---
+++ include/qx/iterate.h
@@ -0,0 +1,74 @@
+/**
+
+    @file      iterate.h
+    @brief     Contains qx::iterate functions along with default filters and adapters for him
+    @details   ~
+    @author    Khrapov
+    @date      23.08.2022
+    @copyright © Nick Khrapov, 2022. All right reserved.
+
+**/
+#pragma once
+
+#include <utility>
+
+namespace qx
+{
+
+namespace adapters
+{
+
+constexpr auto no_change = []<class T>(T&& value)
+{
+    return std::forward<T>(value);
+};
+
+} // namespace adapters
+
+namespace filters
+{
+
+constexpr auto always_true = [](const auto&)
+{
+    return true;
+};
+
+} // namespace filters
+
+/**
+    @brief   Iterate container with filter
+    @details This function is useful when:
+             1. you don't want to use getter for container
+                (for ex. even const vector of shared_ptr will allow the client to make one more shared_ptr)
+             2. you don't want the client to know what type of container you are using
+             3. you want the client to be able to iterate only over a part of the value of an element of the vector
+    @see     code examples in test_iterate.cpp
+    @tparam  Container - container of elements type
+    @tparam  Callable  - callable type. if callable returns false, iteration breaks
+    @tparam  Filter    - filter type. if filter returns true, callable is applied
+    @tparam  Adapter   - adapter type. converts container element type to another type for callable and filter
+    @param   container - container object
+    @param   callable  - callable object
+    @param   filter    - filter object
+    @param   adapter   - adapter object
+**/
+template<
+    class Container,
+    class Callable,
+    class Filter  = decltype(filters::always_true),
+    class Adapter = decltype(adapters::no_change)>
+void iterate(
+    const Container& container,
+    const Callable&  callable,
+    const Filter&    filter  = filters::always_true,
+    const Adapter&   adapter = adapters::no_change)
+{
+    for (const auto& element : container)
+    {
+        auto adaptedElement = adapter(element);
+        if (filter(adaptedElement) && callable(adaptedElement))
+            break;
+    }
+}
+
+} // namespace qx
---
+++ tests/test_iterate.cpp
@@ -0,0 +1,132 @@
+/**
+
+    @file      test_iterate.cpp
+    @author    Khrapov
+    @date      23.08.2022
+    @copyright © Nick Khrapov, 2022. All right reserved.
+
+**/
+#include <common.h>
+
+//V_EXCLUDE_PATH *test_iterate.cpp
+
+#include <qx/iterate.h>
+
+#include <vector>
+
+struct MyStruct
+{
+    int  integer;
+    char character;
+};
+
+class MyClass
+{
+public:
+    MyClass(std::vector<MyStruct> vector) : m_Vector(std::move(vector))
+    {
+    }
+
+    template<class Callable, class Filter = decltype(qx::filters::always_true)>
+    void Iterate(const Callable& callable, const Filter& filter = qx::filters::always_true) const
+    {
+        qx::iterate(m_Vector, callable, filter);
+    }
+
+    template<class Callable, class Filter = decltype(qx::filters::always_true)>
+    void IterateInteger(const Callable& callable, const Filter& filter = qx::filters::always_true) const
+    {
+        qx::iterate(
+            m_Vector,
+            callable,
+            filter,
+            [](const MyStruct& myStruct)
+            {
+                return myStruct.integer;
+            });
+    }
+
+private:
+    std::vector<MyStruct> m_Vector;
+};
+
+TEST(iterate, main)
+{
+    const MyClass myClass({ { -2, 'a' }, { -1, 'b' }, { 0, 'c' }, { 1, 'd' }, { 2, 'e' }, { 4, 'f' }, { 1, 'u' } });
+
+    {
+        std::string str;
+        myClass.Iterate(
+            [&str](const MyStruct& myStruct)
+            {
+                str += myStruct.character;
+                return false;
+            });
+
+        EXPECT_STREQ(str.data(), "abcdefu");
+    }
+
+    {
+        int sum = 0;
+        myClass.IterateInteger(
+            [&sum](int integer)
+            {
+                sum += integer;
+                return false;
+            },
+            [](int integer)
+            {
+                return integer > 0;
+            });
+
+        EXPECT_EQ(sum, 8);
+    }
+
+    {
+        int sum = 0;
+        myClass.IterateInteger(
+            [&sum](int integer)
+            {
+                sum += integer;
+                return true;
+            },
+            [](int integer)
+            {
+                return integer > 0;
+            });
+
+        EXPECT_EQ(sum, 1);
+    }
+
+    {
+        int sum = 0;
+        myClass.IterateInteger(
+            [&sum](int integer)
+            {
+                sum += integer;
+                return false;
+            },
+            [](int integer)
+            {
+                return integer != 0;
+            });
+
+        EXPECT_EQ(sum, 5);
+    }
+
+    {
+        int sum = 0;
+        myClass.IterateInteger(
+            [&sum](int integer)
+            {
+                sum += integer;
+                return true;
+            },
+            [](int integer)
+            {
+                return integer != 0;
+            });
+
+        EXPECT_EQ(sum, -2);
+    }
+}
