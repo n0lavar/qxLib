02464222c44ef809761ab06454b58e6f233bb33f
n0lavar <nick.khrapov@gmail.com>
2022-11-07 23:29:38 +0300

    use predefined build type macro is have

--- include/qx/macros/config.h
+++ include/qx/macros/config.h
@@ -51,10 +51,26 @@
 
 //------------------------------------ build -----------------------------------
 
-#ifdef _DEBUG
-    #define QX_DEBUG   1
-    #define QX_RELEASE 0
-#else
-    #define QX_DEBUG   0
-    #define QX_RELEASE 1
+#ifndef QX_DEBUG
+    #ifdef _DEBUG
+        #define QX_DEBUG 1
+    #else
+        #define QX_DEBUG 0
+    #endif
+#endif
+
+#ifndef QX_RELEASE
+    #ifdef _DEBUG
+        #define QX_RELEASE 0
+    #else
+        #define QX_RELEASE 1
+    #endif
+#endif
+
+#ifndef QX_REL_WITH_DEBUG_INFO
+    #define QX_REL_WITH_DEBUG_INFO 0
+#endif
+
+#ifndef QX_MIN_SIZE_RELEASE
+    #define QX_MIN_SIZE_RELEASE 0
 #endif
