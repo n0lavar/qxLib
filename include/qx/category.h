79a68b9777c28faa557cb2a2a198ad5e8a27f428
n0lavar <nick.khrapov@gmail.com>
2022-12-14 11:50:35 +0300

    change constructor params order

--- include/qx/category.h
+++ include/qx/category.h
@@ -15,7 +15,7 @@
 /**
     @brief Define a category
     @param name - category name
-    @param ...  - optional category description and color
+    @param ...  - optional category color and description
 **/
 #define QX_DEFINE_CATEGORY(name, ...) constexpr qx::category name(#name, ##__VA_ARGS__);
 
@@ -40,13 +40,13 @@ public:
     /**
         @brief  category object constructor
         @param  pszName        - category name. For ex. CatRendering or CatWidgets
-        @param  pszDescription - category description
         @param  color          - color to be used if supported
+        @param  pszDescription - category description
     **/
     constexpr explicit category(
         const char*  pszName,
-        const char*  pszDescription = nullptr,
-        const color& color          = kDefaultColor) noexcept;
+        const color& color          = kDefaultColor,
+        const char*  pszDescription = nullptr) noexcept;
 
     /**
         @brief  Get category name
@@ -67,15 +67,15 @@ public:
     constexpr const color& get_color() const noexcept;
 
 private:
+    const color       m_Color          = kDefaultColor;
     const char* const m_pszName        = nullptr;
     const char* const m_pszDescription = nullptr;
-    const color       m_Color          = kDefaultColor;
 };
 
-constexpr category::category(const char* pszName, const char* pszDescription, const color& color) noexcept
-    : m_pszName(pszName)
+constexpr category::category(const char* pszName, const color& color, const char* pszDescription) noexcept
+    : m_Color(color)
+    , m_pszName(pszName)
     , m_pszDescription(pszDescription)
-    , m_Color(color)
 {
 }
 
