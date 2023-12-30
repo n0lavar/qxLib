447cbd0d974ecf226700ccf94b9d6c3925dcfebd
n0lavar <nick.khrapov@gmail.com>
2023-12-30 14:44:29 +0000

    add more tuple utils

--- include/qx/meta/tuple_utils.h
+++ include/qx/meta/tuple_utils.h
@@ -17,6 +17,10 @@ namespace qx::tuple
 
 // -------------------------------------------------------- join -------------------------------------------------------
 
+/**
+    @struct join
+    @brief  Appends types or another tuple to tuple
+**/
 template<class...>
 struct join
 {
@@ -26,6 +30,12 @@ template<class... first_pack_t, class... second_pack_t>
 struct join<std::tuple<first_pack_t...>, std::tuple<second_pack_t...>>
 {
     using type = std::tuple<first_pack_t..., second_pack_t...>;
+};
+
+template<class... first_pack_t, class second_t, class... optional_args_t>
+struct join<std::tuple<first_pack_t...>, second_t, optional_args_t...>
+{
+    using type = std::tuple<first_pack_t..., second_t, optional_args_t...>;
 };
 
 template<class... args_t>
@@ -57,7 +67,11 @@ struct remove_single<target_t, std::tuple<parameter_t, pack_t...>>
 
 } // namespace details
 
-template<class... pack_t>
+/**
+    @struct remove
+    @brief  Removes all types from the second argument from the first tuple
+**/
+template<class...>
 struct remove
 {
 };
@@ -92,22 +106,110 @@ struct contains<T, std::tuple<>> : std::false_type
 {
 };
 
-template<class T, class U, class... Ts>
-struct contains<T, std::tuple<U, Ts...>> : contains<T, std::tuple<Ts...>>
+template<class T, class U, class... args_t>
+struct contains<T, std::tuple<U, args_t...>> : contains<T, std::tuple<args_t...>>
 {
 };
 
-template<class T, class... Ts>
-struct contains<T, std::tuple<T, Ts...>> : std::true_type
+template<class T, class... args_t>
+struct contains<T, std::tuple<T, args_t...>> : std::true_type
 {
 };
 
 } // namespace details
 
+/**
+    @struct contains
+    @brief  Check that tuple type contains T
+    @tparam tuple_t - std::tuple<> type
+    @tparam T       - type to check
+**/
 template<class tuple_t, class T>
 using contains = details::contains<T, tuple_t>;
 
 template<class tuple_t, class T>
 static constexpr bool contains_v = contains<tuple_t, T>::value;
+
+// ----------------------------------------------------- transform -----------------------------------------------------
+
+/**
+    @struct transform
+    @brief  Get a new tuple type by transforming every type inside a given tuple
+    @tparam tuple_t          - std::tuple<> type
+    @tparam transformation_t - transformation type, for ex. std::add_pointer, std::remove_pointer etc. (without `_t`!)
+**/
+template<class tuple_t, template<class T> class transformation_t>
+struct transform;
+
+template<template<class T> class transformation_t, class... args_t>
+struct transform<std::tuple<args_t...>, transformation_t>
+{
+    using type = std::tuple<typename transformation_t<args_t>::type...>;
+};
+
+template<class tuple_t, template<class T> class transformation_t>
+using transform_t = typename transform<tuple_t, transformation_t>::type;
+
+// ------------------------------------------------------- index -------------------------------------------------------
+
+/**
+    @struct index
+    @brief  Get an index of the first occurrence of the given type
+    @tparam tuple_t - std::tuple<> type
+    @tparam T       - type to check
+**/
+template<class tuple_t, class T>
+struct index;
+
+template<class T, class... args_t>
+struct index<std::tuple<T, args_t...>, T>
+{
+    static constexpr size_t value = 0;
+};
+
+template<class T, class U, class... args_t>
+struct index<std::tuple<U, args_t...>, T>
+{
+    static constexpr size_t value = 1 + index<std::tuple<args_t...>, T>::value;
+};
+
+template<class tuple_t, class T>
+constexpr size_t index_v = index<tuple_t, T>::value;
+
+// ------------------------------------------------------ iterate ------------------------------------------------------
+
+/**
+    @brief  Iterate over a tuple with a callable that receives a type along with a its index 
+    @tparam tuple_t         - std::tuple<> type
+    @tparam type_callable_t - callable type
+    @param  callable        - callable object
+
+    @code
+    using tuple_type = std::tuple<float, int, std::string, unsigned, size_t>;
+    qx::tuple::iterate<tuple_type>(
+        []<class T, size_t I>()
+        {
+            // ...
+        });
+    @endcode
+**/
+template<class tuple_t, class type_callable_t>
+constexpr void iterate(const type_callable_t& callable)
+{
+    // we need to add a pointer because we can't compile a tuple with an abstract class type
+    using tuple_pointer_type = transform_t<tuple_t, std::add_pointer>;
+
+    auto temp_callable = [&callable]<class T>(const T&)
+    {
+        callable.template operator()<std::remove_pointer_t<T>, index_v<tuple_pointer_type, T>>();
+    };
+
+    std::apply(
+        [&temp_callable](auto&&... args)
+        {
+            ((temp_callable(args)), ...);
+        },
+        tuple_pointer_type());
+}
 
 } // namespace qx::tuple
--- tests/test_tuple_utils.cpp
+++ tests/test_tuple_utils.cpp
@@ -17,6 +17,8 @@ using TTuple1 = std::tuple<int>;
 using TTuple2 = std::tuple<float, std::tuple<int>>;
 using TTuple3 = std::tuple<float, float, std::string>;
 
+
+
 // -------------------------------------------------- qx::tuple::join --------------------------------------------------
 
 using TJoin00 = qx::tuple::join_t<TTuple0, TTuple0>;
@@ -54,6 +56,17 @@ static_assert(std::is_same_v<TJoin30, std::tuple<float, float, std::string>>);
 static_assert(std::is_same_v<TJoin31, std::tuple<float, float, std::string, int>>);
 static_assert(std::is_same_v<TJoin32, std::tuple<float, float, std::string, float, std::tuple<int>>>);
 static_assert(std::is_same_v<TJoin33, std::tuple<float, float, std::string, float, float, std::string>>);
+
+using TJoin3t0 = qx::tuple::join_t<TTuple3, int>;
+using TJoin3t1 = qx::tuple::join_t<TTuple3, float>;
+using TJoin3t2 = qx::tuple::join_t<TTuple3, int, int>;
+using TJoin3t3 = qx::tuple::join_t<TTuple3, std::string>;
+static_assert(std::is_same_v<TJoin3t0, std::tuple<float, float, std::string, int>>);
+static_assert(std::is_same_v<TJoin3t1, std::tuple<float, float, std::string, float>>);
+static_assert(std::is_same_v<TJoin3t2, std::tuple<float, float, std::string, int, int>>);
+static_assert(std::is_same_v<TJoin3t3, std::tuple<float, float, std::string, std::string>>);
+
+
 
 // ------------------------------------------------- qx::tuple::remove -------------------------------------------------
 
@@ -93,6 +106,8 @@ static_assert(std::is_same_v<TRemove31, std::tuple<float, float, std::string>>);
 static_assert(std::is_same_v<TRemove32, std::tuple<std::string>>);
 static_assert(std::is_same_v<TRemove33, std::tuple<>>);
 
+
+
 // ------------------------------------------------ qx::tuple::contains ------------------------------------------------
 
 static_assert(!qx::tuple::contains_v<TTuple0, int>);
@@ -118,3 +133,74 @@ static_assert(qx::tuple::contains_v<TTuple3, float>);
 static_assert(!qx::tuple::contains_v<TTuple3, std::tuple<int>>);
 static_assert(qx::tuple::contains_v<TTuple3, std::string>);
 static_assert(!qx::tuple::contains_v<TTuple3, size_t>);
+
+
+
+// ------------------------------------------------ qx::tuple::transform -----------------------------------------------
+
+using TTransformAddPointer0 = qx::tuple::transform_t<TTuple0, std::add_pointer>;
+using TTransformAddPointer1 = qx::tuple::transform_t<TTuple1, std::add_pointer>;
+using TTransformAddPointer2 = qx::tuple::transform_t<TTuple2, std::add_pointer>;
+using TTransformAddPointer3 = qx::tuple::transform_t<TTuple3, std::add_pointer>;
+static_assert(std::is_same_v<TTransformAddPointer0, std::tuple<>>);
+static_assert(std::is_same_v<TTransformAddPointer1, std::tuple<int*>>);
+static_assert(std::is_same_v<TTransformAddPointer2, std::tuple<float*, std::tuple<int>*>>);
+static_assert(std::is_same_v<TTransformAddPointer3, std::tuple<float*, float*, std::string*>>);
+
+using TTransformAddLRef0 = qx::tuple::transform_t<TTuple0, std::add_lvalue_reference>;
+using TTransformAddLRef1 = qx::tuple::transform_t<TTuple1, std::add_lvalue_reference>;
+using TTransformAddLRef2 = qx::tuple::transform_t<TTuple2, std::add_lvalue_reference>;
+using TTransformAddLRef3 = qx::tuple::transform_t<TTuple3, std::add_lvalue_reference>;
+static_assert(std::is_same_v<TTransformAddLRef0, std::tuple<>>);
+static_assert(std::is_same_v<TTransformAddLRef1, std::tuple<int&>>);
+static_assert(std::is_same_v<TTransformAddLRef2, std::tuple<float&, std::tuple<int>&>>);
+static_assert(std::is_same_v<TTransformAddLRef3, std::tuple<float&, float&, std::string&>>);
+
+using TTransformAddConst0 = qx::tuple::transform_t<TTuple0, std::add_const>;
+using TTransformAddConst1 = qx::tuple::transform_t<TTuple1, std::add_const>;
+using TTransformAddConst2 = qx::tuple::transform_t<TTuple2, std::add_const>;
+using TTransformAddConst3 = qx::tuple::transform_t<TTuple3, std::add_const>;
+static_assert(std::is_same_v<TTransformAddConst0, std::tuple<>>);
+static_assert(std::is_same_v<TTransformAddConst1, std::tuple<const int>>);
+static_assert(std::is_same_v<TTransformAddConst2, std::tuple<const float, const std::tuple<int>>>);
+static_assert(std::is_same_v<TTransformAddConst3, std::tuple<const float, const float, const std::string>>);
+
+using TTransformAddVolatile0 = qx::tuple::transform_t<TTuple0, std::add_volatile>;
+using TTransformAddVolatile1 = qx::tuple::transform_t<TTuple1, std::add_volatile>;
+using TTransformAddVolatile2 = qx::tuple::transform_t<TTuple2, std::add_volatile>;
+using TTransformAddVolatile3 = qx::tuple::transform_t<TTuple3, std::add_volatile>;
+static_assert(std::is_same_v<TTransformAddVolatile0, std::tuple<>>);
+static_assert(std::is_same_v<TTransformAddVolatile1, std::tuple<volatile int>>);
+static_assert(std::is_same_v<TTransformAddVolatile2, std::tuple<volatile float, volatile std::tuple<int>>>);
+static_assert(std::is_same_v<TTransformAddVolatile3, std::tuple<volatile float, volatile float, volatile std::string>>);
+
+
+
+// -------------------------------------------------- qx::tuple::index -------------------------------------------------
+
+static_assert(qx::tuple::index_v<TTuple1, int> == 0);
+static_assert(qx::tuple::index_v<TTuple2, float> == 0);
+static_assert(qx::tuple::index_v<TTuple3, std::string> == 2);
+
+
+
+// ------------------------------------------------- qx::tuple::iterate ------------------------------------------------
+
+TEST(tuple, iterate)
+{
+    using TTuple        = std::tuple<float, int, std::string, unsigned, size_t>;
+    size_t nInvocations = 0;
+    qx::tuple::iterate<TTuple>(
+        [&nInvocations]<class T, size_t I>()
+        {
+            static_assert(I != 0 || std::is_same_v<T, float>);
+            static_assert(I != 1 || std::is_same_v<T, int>);
+            static_assert(I != 2 || std::is_same_v<T, std::string>);
+            static_assert(I != 3 || std::is_same_v<T, unsigned>);
+            static_assert(I != 4 || std::is_same_v<T, size_t>);
+            static_assert(I < 5);
+            ++nInvocations;
+        });
+
+    EXPECT_EQ(nInvocations, 5);
+}
