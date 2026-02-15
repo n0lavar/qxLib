6869bdadcd6be6e89d658ff8179842e66c129917
n0lavar <n0lavar@users.noreply.github.com>
2026-02-15 19:04:34 +0000

    deploy: be9f082394d6fe32e1f1037857b5318446cae589

--- search/all_16.js
+++ search/all_16.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['width_863',['width',['../classqx_1_1basic__rect.html#a298cb9e613419ea70bf467513ca07ce9',1,'qx::basic_rect']]],
   ['windows_2eh_864',['windows.h',['../windows_8h.html',1,'']]],
-  ['write_865',['write',['../classqx_1_1terminal__color.html#a76fd9ee2f05231f6c69861d6c75c06e3',1,'qx::terminal_color']]]
+  ['write_865',['write',['../classqx_1_1terminal__color.html#a8b451c1737e63bfe9a15d6e17b0d0737',1,'qx::terminal_color']]]
 ];
